3aae513fe44207c8ef15b3780611da50b865d63f
commixon <commixon@gmail.com>
2018-05-04 01:03:52 +0300

    Publish to rabbitmq

--- relay/amqp.go
+++ relay/amqp.go
@@ -1,8 +1,52 @@
 package relay
 
-import "log"
+import (
+  "log"
+
+  "encoding/json"
+  "github.com/streadway/amqp"
+)
 
 func pushToAmqp() {
-	log.Println(amqpURL)
-  log.Println(metering)
+  conn, err := amqp.Dial(amqpURL)
+  if err != nil {
+    log.Fatalln(err)
+  }
+  defer conn.Close()
+
+  ch, err := conn.Channel()
+	if err != nil {
+    log.Fatalln(err)
+  }
+	defer ch.Close()
+
+  body, err := json.Marshal(metering)
+  if err != nil {
+    return
+  }
+
+  q, err := ch.QueueDeclare(
+    "metering", // name
+    false,   // durable
+    false,   // delete when unused
+    false,   // exclusive
+    false,   // no-wait
+    nil,     // arguments
+  )
+
+  err = ch.Publish(
+    "",     // exchange
+    q.Name, // routing key
+    false,  // mandatory
+    false,  // immediate
+    amqp.Publishing {
+      ContentType: "text/plain",
+      Body:        body,
+    })
+
+  if err != nil {
+    log.Println("Error while publishing:")
+    log.Println(err)
+  }
+
 }
